f2102e83ffc37cf331acfa67e0aba451d7a3b2e6
BognaLew <100587236+BognaLew@users.noreply.github.com>
2024-09-21 21:23:10 +0200

    greedy agent draft

---
+++ src/agents/greedy.py
@@ -0,0 +1,23 @@
+from carcassonne_engine.models import SerializedGameWithID
+
+from . import Agent
+
+
+class GreedyAgent(Agent):
+    def __init__(self, id: int) -> None:
+        super().__init__()
+        self.ID = id
+
+    def policy(self, prob: float, action: SerializedGameWithID, q: float) -> float:
+        """
+        Policy evaluation function.
+        Action is assigned it's point value.
+        """
+        return float(action.game.players[self.ID].Score)
+
+    def rollout(self, action: SerializedGameWithID) -> float:
+        """
+        Simulates outcome of specific state
+        Action is assigned it's point value.
+        """
+        return float(action.game.players[self.ID].Score)
--- test/game_test.py
+++ test/game_test.py
@@ -1,6 +1,9 @@
-from src.agents import RandomAgent
+from src.agents import GreedyAgent, RandomAgent
 from src.game import Game
 
 
 def test_random_agent_game(engine, small_tileset):
     Game(engine, small_tileset).run(RandomAgent(), RandomAgent())
+
+def test_greedy_agent_game(engine, small_tileset):
+    Game(engine, small_tileset).run(GreedyAgent(), GreedyAgent())
